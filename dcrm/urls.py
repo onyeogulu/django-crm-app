01f067cb88b8cc5179af44a4db5163136d5ad683
Onyeogulu Tochukwu Rowland <49307590+onyeogulu@users.noreply.github.com>
2023-09-27 22:35:10 +0100

    made several changes

---
+++ dcrm/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', views.login_user, name='login'), # type: ignore
+    path('logout', views.logout_user, name='logout'), # type: ignore
+    path('register', views.register_user, name='register'), # type: ignore
+]
